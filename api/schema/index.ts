f546f1dbefc09d912c1021b64a5a95857446bb70
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-02-28 13:03:26 +0200

    refactor: remove tests

--- api/schema/index.ts
+++ api/schema/index.ts
@@ -1,7 +1,28 @@
-import 'graphql-import-node'
+import { gql } from 'apollo-server'
 import { makeExecutableSchema } from 'graphql-tools'
 import resolvers from './resolvers'
-import * as typeDefs from './schema.graphql'
+
+const typeDefs = gql`
+  type User {
+    email: String!
+    id: ID!
+    name: String
+    posts: [Post!]!
+  }
+
+  type Post {
+    by: String
+    title: String
+    text: String
+    score: String
+  }
+
+  type Query {
+    me: User
+    feed: [Post!]!
+    post(storyId: String!): Post
+  }
+`
 
 export const schema = makeExecutableSchema({
   resolvers,
