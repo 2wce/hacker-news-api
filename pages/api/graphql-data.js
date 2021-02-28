360aa0fcbc181938b199f462a7384df92e79a8bf
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-02-28 13:23:08 +0200

    feat: next.js

---
+++ pages/api/graphql-data.js
@@ -0,0 +1,53 @@
+import { gql } from "apollo-server-micro";
+import axios from "axios";
+import resolvers from "./resolvers";
+
+const getStory = async (storyId) => {
+  const result = await axios.get(`${storyUrl + storyId}.json`);
+  return selectFields(result.data);
+};
+
+const getStoryIds = async () => {
+  const result = await axios.get(newStoriesUrl);
+  return result.data;
+};
+
+const typeDefs = gql`
+  type Post {
+    by: String
+    title: String
+    text: String
+    score: String
+  }
+
+  type Query {
+    feed: [Post!]!
+    post(storyId: String!): Post
+  }
+`;
+
+const resolvers = {
+  Query: {
+    feed: async (parent, args, ctx) => {
+      const storyIds = await getStoryIds();
+      const data = await Promise.all(storyIds.map((id) => getStory(id)));
+
+      return data;
+    },
+    post: (parent, { storyId }, ctx) => {
+      return getStory(storyId);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const handler = server.createHandler({ path: "/api/graphql-data" });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
